addfef489208eb4c5f1b92a6e7e5402f89798011
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-18 22:33:01 +0530

    getAlltodoes added

--- src/main/java/com/ritesh/boot/todo/service/TodoService.java
+++ src/main/java/com/ritesh/boot/todo/service/TodoService.java
@@ -4,16 +4,23 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.ritesh.boot.todo.template.Todo;
 
+@Service
 public class TodoService {
 
-	private static List<Todo> todoos = new ArrayList<Todo>();
-	
+	private static List<Todo> todoes = new ArrayList<Todo>();
+
 	static {
-		todoos.add(new Todo(1, "Ritesh", "Learn Spring Boot MVC ", LocalDate.now().plusMonths(1), false));
-		todoos.add(new Todo(1, "Ritesh", "Brindavan Holi ", LocalDate.now().plusDays(30), false));
-		todoos.add(new Todo(1, "Ritesh", "Learn Spring Boot Security ", LocalDate.now().plusMonths(1), false));
-		
+		todoes.add(new Todo(1, "Ritesh", "Learn Spring Boot MVC ", LocalDate.now().plusMonths(1), false));
+		todoes.add(new Todo(1, "Ritesh", "Brindavan Holi ", LocalDate.now().plusDays(30), false));
+		todoes.add(new Todo(1, "Ritesh", "Learn Spring Boot Security ", LocalDate.now().plusMonths(1), false));
+
+	}
+	public List<Todo> getAlltodoes()
+	{
+		return todoes;
 	}
 }
