8fe1eb2c8c019d4489d6f05c7eb4b9164da3c19e
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-20 03:03:14 +0530

    addtodo servided coded

--- src/main/java/com/ritesh/boot/todo/service/TodoService.java
+++ src/main/java/com/ritesh/boot/todo/service/TodoService.java
@@ -12,15 +12,19 @@ import com.ritesh.boot.todo.template.Todo;
 public class TodoService {
 
 	private static List<Todo> todoes = new ArrayList<Todo>();
-
+	private static int countTodo;
 	static {
-		todoes.add(new Todo(1, "Ritesh", "Learn Spring Boot MVC ", LocalDate.now().plusMonths(1), false));
-		todoes.add(new Todo(1, "Ritesh", "Brindavan Holi ", LocalDate.now().plusDays(30), false));
-		todoes.add(new Todo(1, "Ritesh", "Learn Spring Boot Security ", LocalDate.now().plusMonths(1), false));
-
+		todoes.add(new Todo(++countTodo, "Ritesh", "Learn Spring Boot MVC ", LocalDate.now().plusMonths(1), false));
+		todoes.add(new Todo(++countTodo, "Ritesh", "Brindavan Holi ", LocalDate.now().plusDays(30), false));
+		todoes.add(
+				new Todo(++countTodo, "Ritesh", "Learn Spring Boot Security ", LocalDate.now().plusMonths(1), false));
 	}
-	public List<Todo> getAlltodoes()
-	{
+
+	public List<Todo> getAlltodoes() {
 		return todoes;
+	}
+
+	public void addtodo(String user, String description, LocalDate targetDate, boolean done) {
+		todoes.add(new Todo(++countTodo, user, description, targetDate, done));
 	}
 }
