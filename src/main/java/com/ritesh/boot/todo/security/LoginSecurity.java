594d4022818601da5f29f456835770c139041f86
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-20 02:59:33 +0530

    user validation completed.

--- src/main/java/com/ritesh/boot/todo/security/LoginSecurity.java
+++ src/main/java/com/ritesh/boot/todo/security/LoginSecurity.java
@@ -3,7 +3,6 @@ package com.ritesh.boot.todo.security;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.jasper.tagplugins.jstl.core.ForEach;
 import org.springframework.stereotype.Service;
 
 import com.ritesh.boot.todo.template.User;
@@ -11,17 +10,19 @@ import com.ritesh.boot.todo.template.User;
 @Service
 public class LoginSecurity {
 
-	public static List<User> users  = new ArrayList<User>();
+	public static List<User> users = new ArrayList<User>();
 	static {
-		users.add(new User("Ritesh","Ritesh@123"));
-		users.add(new User("user","123"));
+		users.add(new User("Ritesh", "Ritesh@123"));
+		users.add(new User("user", "123"));
 	}
-	
-	public boolean validateUser(String u,String p) {
+
+	public boolean validateUser(String u, String p) {
 		for (User user : users) {
-			
+			if (user.userId.equalsIgnoreCase(u) && user.password.equals(p)) {
+				return true;
+			}
 		}
-		return true;
+		return false;
 	}
 
 }
