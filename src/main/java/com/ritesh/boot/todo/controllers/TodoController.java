e0b042e8fbf83545185e0df0a6d5325186cf3f2b
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-02-20 03:18:35 +0530

    added path for adding new todo and organized controller, added comments.

--- src/main/java/com/ritesh/boot/todo/controllers/TodoController.java
+++ src/main/java/com/ritesh/boot/todo/controllers/TodoController.java
@@ -1,5 +1,6 @@
 package com.ritesh.boot.todo.controllers;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,32 +8,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.ritesh.boot.todo.security.LoginSecurity;
 import com.ritesh.boot.todo.service.TodoService;
 import com.ritesh.boot.todo.template.Todo;
 
 //http://localhost:8080/TodoManagement/login
 
 @Controller
+@SessionAttributes("userid")
 public class TodoController {
 
 	@Autowired
 	TodoService todoService;
 
-	@Autowired
-	LoginSecurity loginSecurity;
-
-	@RequestMapping(value = "")
-	public String hello(ModelMap Model) {
-		return "Home";
-	}
-
-	@RequestMapping(value = "/login")
-	public String login(ModelMap Model) {
-		return "login";
-	}
-
+	// to see list of toDoes
 	@RequestMapping("/todoes")
 	public ModelMap getAllTodoes(ModelMap mm) {
 		List<Todo> listTodo = todoService.getAlltodoes();
@@ -40,11 +31,24 @@ public class TodoController {
 		return mm;
 	}
 
-	//@RequestMapping(path = "/todoes",method = RequestMethod.GET)
+	// @RequestMapping(path = "/todoes",method = RequestMethod.POST)
 	public ModelMap postAllTodoes(ModelMap mm) {
-		
+
 		List<Todo> listTodo = todoService.getAlltodoes();
 		mm.addAttribute("todoes", listTodo);
 		return mm;
+	}
+
+	// To show Add todo page
+	@RequestMapping("/add-todo")
+	public String addTodo() {
+		return "addTodo";
+	}
+
+	// to show updated todo
+	@RequestMapping(path = "/add-todo", method = RequestMethod.POST)
+	public String addNewTodo(@RequestParam("description") String Desc, ModelMap mm) {
+		todoService.addtodo((String) mm.get("userid"), Desc, LocalDate.now().plusMonths(1), false);
+		return "redirect:/todoes";
 	}
 }
