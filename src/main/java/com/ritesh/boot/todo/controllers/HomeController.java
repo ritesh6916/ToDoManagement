4ecbc0d8d5edabf85d73d6ca30ccdc700a08d3bf
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-01 00:41:52 +0530

    manual security checking removed.

--- src/main/java/com/ritesh/boot/todo/controllers/HomeController.java
+++ src/main/java/com/ritesh/boot/todo/controllers/HomeController.java
@@ -1,34 +1,29 @@
 package com.ritesh.boot.todo.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-
-import com.ritesh.boot.todo.security.LoginSecurity;
 
 @Controller
 @SessionAttributes("userid")
 public class HomeController {
 
-	@Autowired
-	LoginSecurity loginSecurity;
+	/*
+	 * @Autowired LoginSecurity loginSecurity;
+	 */
 
-	@RequestMapping(value = "")
-	public String hello() {
-		return "login";
+	@RequestMapping(value = "/")
+	public String welcome(ModelMap mm) {
+		mm.put("userid", "Ritesh6916");
+		return "Home";
 	}
 
-	@RequestMapping(value = "/home", method = RequestMethod.GET)
-	public String login(@RequestParam("userid") String userid, @RequestParam("password") String password, ModelMap mm) {
-		if (loginSecurity.validateUser(userid, password)) {
-			mm.put("userid", userid);
-			return "Home";
-		}
-		mm.addAttribute("Error", "UserID or Password Incorrect");
-		return "login";
-	}
+	/*
+	 * @RequestMapping(value = "/home", method = RequestMethod.GET) public String
+	 * login(@RequestParam("userid") String userid, @RequestParam("password") String
+	 * password, ModelMap mm) { if (loginSecurity.validateUser(userid, password)) {
+	 * mm.put("userid", userid); //mm.addAttribute("userid", userid); return "Home";
+	 * } mm.addAttribute("Error", "UserID or Password Incorrect"); return "login"; }
+	 */
 }
