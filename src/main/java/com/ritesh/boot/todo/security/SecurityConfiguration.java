0a1f8f0f9df99a54844e976915d84cc53aa34e2f
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-02 01:36:30 +0530

    Configured Security as per H2 DB.

--- src/main/java/com/ritesh/boot/todo/security/SecurityConfiguration.java
+++ src/main/java/com/ritesh/boot/todo/security/SecurityConfiguration.java
@@ -4,11 +4,15 @@ import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
@@ -38,4 +42,17 @@ public class SecurityConfiguration {
 		return new BCryptPasswordEncoder();
 	}
 
+	// default -> all url protected & login form
+	// To use H2 console via web :
+	// CSRF shuld be disabled & Frames are enabled
+
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+		http.formLogin(withDefaults());
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		return http.build();
+	}
 }
