c587133943c938f828ffc46efedf59a56823d332
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-02 03:19:11 +0530

    added findByUsername to get data by username.

--- src/main/java/com/ritesh/boot/todo/persistence/TodoRepository.java
+++ src/main/java/com/ritesh/boot/todo/persistence/TodoRepository.java
@@ -1,9 +1,11 @@
 package com.ritesh.boot.todo.persistence;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ritesh.boot.todo.template.Todo;
 
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
-
+	public List<Todo> findByUsername(String username);
 }
