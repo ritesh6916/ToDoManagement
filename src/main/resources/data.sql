5eed75eca4ff6b2cccbc6b37ef89d2b9ddc7a71a
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-02 02:21:45 +0530

    script to insert default data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1001, 'Ritesh', 'Get AWS Developer Certified', CURRENT_DATE(), false);
+
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1002, 'Ritesh', 'Learn Spring Boot', CURRENT_DATE(), false);
+
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1003, 'Ritesh', 'Learn to work with REST-APIs', CURRENT_DATE(), false);
+
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1004, 'Ritesh', 'Set a target for Hibernate', CURRENT_DATE(), false);
+
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1005, 'Dummy', 'Just a dummy recored for Dummy user', CURRENT_DATE(), false);
+
+insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(1006, 'Ritesh', 'Brindavan Holi', CURRENT_DATE(), false);
