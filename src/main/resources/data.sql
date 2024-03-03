54b0ed9ad10b310a28a36036714dbfcae60ee4e2
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-03 20:27:31 +0530

    changed IDs

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,17 +1,17 @@
 insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values(1001, 'Ritesh', 'Get AWS Developer Certified', CURRENT_DATE(), false);
+values(1, 'Ritesh', 'Get AWS Developer Certified', CURRENT_DATE(), false);
 
 insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values(1002, 'Ritesh', 'Learn Spring Boot', CURRENT_DATE(), false);
+values(2, 'Ritesh', 'Learn Spring Boot', CURRENT_DATE(), false);
 
 insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values(1003, 'Ritesh', 'Learn to work with REST-APIs', CURRENT_DATE(), false);
+values(3, 'Ritesh', 'Learn to work with REST-APIs', CURRENT_DATE(), false);
 
 insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values(1004, 'Ritesh', 'Set a target for Hibernate', CURRENT_DATE(), false);
+values(4, 'Ritesh', 'Set a target for Hibernate', CURRENT_DATE(), false);
 
 insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values(1005, 'Dummy', 'Just a dummy recored for Dummy user', CURRENT_DATE(), false);
+values(5, 'Dummy', 'Just a dummy recored for Dummy user', CURRENT_DATE(), false);
 
 insert into TODO (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
-values(1006, 'Ritesh', 'Brindavan Holi', CURRENT_DATE(), false);
+values(6, 'Ritesh', 'Brindavan Holi', CURRENT_DATE(), false);
