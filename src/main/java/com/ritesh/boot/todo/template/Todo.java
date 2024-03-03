ddb2e274b8f777f4734006f257d2839117234c29
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-03 20:24:50 +0530

    modified to generate ID starting from 7.

--- src/main/java/com/ritesh/boot/todo/template/Todo.java
+++ src/main/java/com/ritesh/boot/todo/template/Todo.java
@@ -2,14 +2,22 @@ package com.ritesh.boot.todo.template;
 
 import java.time.LocalDate;
 
+import org.springframework.stereotype.Service;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.validation.constraints.Size;
 
+@Service
 @Entity
+@SequenceGenerator(name = "Id_gen", sequenceName = "Id_gen", initialValue = 7)
 public class Todo {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO, generator = "Id_gen")
 	private int id;
 	private String username;
 
