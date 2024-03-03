e6f345c28c02e98d5bcbb1f5bb769778a75a2ff0
Ritesh singh <58039285+ritesh6916@users.noreply.github.com>
2024-03-03 20:23:26 +0530

    added @Repository

--- src/main/java/com/ritesh/boot/todo/persistence/TodoRepository.java
+++ src/main/java/com/ritesh/boot/todo/persistence/TodoRepository.java
@@ -3,9 +3,11 @@ package com.ritesh.boot.todo.persistence;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ritesh.boot.todo.template.Todo;
 
+@Repository
 public interface TodoRepository extends JpaRepository<Todo, Integer> {
 	public List<Todo> findByUsername(String username);
 }
